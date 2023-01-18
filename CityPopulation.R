6abb85fdbf29a4fa3d39777e7563bb0e5153248b
Julie Nijs <107923146+nysju@users.noreply.github.com>
2023-01-18 11:07:53 +0100

    Update CityPopulation.R

--- CityPopulation.R
+++ CityPopulation.R
@@ -9,18 +9,24 @@ library(car)
 library(stylo)
 
 #### read the data ####
+
+# read the dataset with the population sizes:
 Datasteden <- read.csv(file = "StedenBelgiëNederland19eEn20eEeuw.csv", 
                        header = TRUE)
 
+# create histogram of the amount of cities per population size in Datasteden:
 hist(Datasteden$BAIy1850, 
      xlab='Populatiegrootte', 
      ylab='Aantal steden',
      main='Histogram van de steden per populatiegrootte')
+
+# same frequencies but log-transformed:
 hist(log(Datasteden$BAIy1850),
      xlab='Populatiegrootte op de logaritmische schaal', 
      ylab='Aantal steden',
      main='Histogram van de steden per populatiegrootte (loggetransformeerd)')
 
+# read the data of the hortative alternation: 
 Verandering <- read_xlsx("The hortative alternation.xlsx", 
                          col_names = TRUE)
 
@@ -29,8 +35,8 @@ Merged <- left_join(Verandering, Datasteden, by = c("BIRTHPLACE" = "CITY"))
 
 # throw away rows without birthplace:
 Merged <- Merged %>% drop_na(BAIy1850) 
-Merged$Change <- as.factor(Merged$Change) 
-Merged <- droplevels(Merged[!Merged$Change=="NA",])
+Merged$Change <- as.factor(Merged$Change) # recode variable Change to a factor
+Merged <- droplevels(Merged[!Merged$Change=="NA",]) # drop NA values
 
 # add log transformed population counts to the data set:
 symbox(Merged$BAIy1850)
@@ -45,7 +51,7 @@ plot(allEffects(Model))
   
 Merged$Change2 <- as.numeric(Merged$Change)-1 # rescale to numeric for ggplot
 
-# data visualisation:
+# data visualisation: hortative alternation over time
 print(ggplot(data = Merged, aes(x = year, y = Change2)) +
         geom_smooth(method = "glm", 
                     method.args = list(family = binomial), 
@@ -56,39 +62,33 @@ print(ggplot(data = Merged, aes(x = year, y = Change2)) +
         scale_y_continuous(limits = c(0,1), 
                             n.breaks = 10)+
         theme_bw())
-  
-print(ggplot(data = Merged, aes(x = logBAIy1850, y = Change2)) +
-        geom_smooth(method = "glm", 
-                    method.args = list(family = binomial), 
-                    colour = "black")+
-        scale_y_continuous(limits = c(0,1), 
-                            n.breaks = 10)+
-        labs(x="Population", y="") +
-        theme_bw())
-
 
 #### Analysis per bin of cities: 3 bins ####
 
-# create bins:
+# create 3 bins based on population counts:
+# not log-transformed:
 Merged <- Merged %>% mutate(BAIy1850_bin_3 = cut_interval(Merged$BAIy1850, 
-                                                          n = 3)) # make bins of the population counts
+                                                          n = 3)) 
+# log-transformed:
 Merged <- Merged %>% mutate(logBAIy1850_bin_3 = cut_interval(Merged$logBAIy1850, 
                                                              n = 3))
 
-# histograms
-hist(Merged$BAIy1850)
-hist(Merged$logBAIy1850)
+# histograms of the amount of cities per population size in the hortative dataset:
+hist(Merged$BAIy1850) # not log-transformed: huge gap in the distribution
+hist(Merged$logBAIy1850) # log-transformed:  nicer distribution
 
 # calculate slopes per bin of cities:
 
+# function calculates slopes and standard errors and plots 
 my_lm <- function(df) {
-  m <-(glm(Change ~ year, data = df, family = binomial(logit)))
-  print(summary(m))
-  summary_m <- summary(m)
-  slopes <- summary_m$coefficients[2, 1]
-  std_errors <- summary_m$coefficients[2, 2]
-  slopes_df <- expand.grid(slopes, std_errors)
+  m <-(glm(Change ~ year, data = df, family = binomial(logit))) # make logistic regression model
+  print(summary(m)) # check model
+  summary_m <- summary(m) # store the summary of the model
+  slopes <- summary_m$coefficients[2, 1] # get the slope
+  std_errors <- summary_m$coefficients[2, 2] # get the standard error
+  slopes_df <- expand.grid(slopes, std_errors) # store slope and st error in dataframe
   df$Change2 <- as.numeric(df$Change)-1 # rescale to numeric for ggplot
+  # make plot:
   print(ggplot(data = df, aes(x = year, y = Change2)) +
           geom_smooth(method = "glm", 
                       method.args = list(family = binomial), 
@@ -101,20 +101,22 @@ my_lm <- function(df) {
           theme_bw())
   return(slopes_df)
 }
-slopes_bin3 <- by(Merged, Merged$logBAIy1850_bin_3, my_lm)
 
-df_bin3 <- do.call(rbind, slopes_bin3)
+# calculate slope and standard deviation and plot slope per bin of cities (log-transformed):
+slopes_bin3 <- by(Merged, Merged$logBAIy1850_bin_3, my_lm) 
+
+df_bin3 <- do.call(rbind, slopes_bin3) # make dataframe with slopes and standard deviations
 
 # plotting:
 
+# calculate 1.95 confidence interval based on standard error:
 ymin <- df_bin3$Var1-(1.95*df_bin3$Var2)
 ymax <- (1.95*df_bin3$Var2)+df_bin3$Var1
 
-p <- ggplot(df_bin3, aes(c("[0.693,2.27]", "[2.27,3.84]", "[3.84,5.42]"), Var1, 
+print(ggplot(df_bin3, aes(c("[0.693,2.27]", "[2.27,3.84]", "[3.84,5.42]"), Var1, 
             ymin = Var1-(1.95*Var2), ymax = (1.95*Var2)+Var1))+
   geom_point()+
   geom_errorbar()+
   labs(x="Groepering per populatiegrootte van de steden", 
        y="Richtingscoëfficiënten", 
-       title="De hortatiefalternantie")
-p
+       title="De hortatiefalternantie"))
